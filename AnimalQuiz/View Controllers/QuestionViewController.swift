93c0c9e48e3b6c456bcf5ba3a98ad3de5f4498bf
Artem Tkachev <tkachevforios@icloud.com>
2021-10-29 21:06:07 +0300

    Initial Commit

---
+++ AnimalQuiz/View Controllers/QuestionViewController.swift
@@ -0,0 +1,12 @@
+//
+//  QuestionViewController.swift
+//  AnimalQuiz
+//
+//  Created by Artem Tkachev on 29.10.21.
+//
+
+import UIKit
+
+class QuestionViewController: UIViewController {
+    
+}
---
+++ AnimalQuiz/View Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnimalQuiz
+//
+//  Created by Artem Tkachev on 29.10.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
